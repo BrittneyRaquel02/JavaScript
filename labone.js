ae032d1c4d567d4976ce7f4b7bd23719926c7b7e
Brittney Vaughn <34939860+BrittneyRaquel02@users.noreply.github.com>
2018-01-30 19:11:28 -0500

    Update labone.js
    went over in class with a classmate.  this is no longer an infinite loop

--- labone.js
+++ labone.js
@@ -6,23 +6,46 @@ if (userInput === "yes"){
   console.log("What is your name?");
 }
 else {
-  console.log("Bye!")
+  console.log("Bye!");
 }
 
-var userName  = prompt ("name")
-var enemyName = "Almighty Grant"
-var userHealth = "40"
-var enemyHealth= "10"
-var wins= "0"
+var userName  = prompt ("name");
+var enemyName = "Almighty Grant";
+var userHealth = "40";
+var enemyHealth = "10";
+var wins = "0";
 
-while(userHealth >0 && wins <3)
-  {if(enemyHealth <=0);}
-  {  wins++
-  enemyHealth = 10; 
+while(userHealth >0 && wins <3){
+ var damage=getRandomInt(3);
+    userHealth = userHealth-damage;
+    console.log("You lost"+ damage + "points");
+    damage = getRandomInt(3);
+    enemyHealth = enemyHealth-damage;
+    console.log(`${enemyName} lost ${damage} points`);
+    
+    
+    
+    if (enemyHealth <=0){
+    wins = wins+1;
+    enemyHealth = 10
+      
+    }
+    
+    
 }
-{Math.floor((Math.random() * 2) + 1);}
-//repl.it is giving an error for an infinite loop? I cannot seem to figure out why
-if(enemyHealth === "0" && wins === "3");
-console.log("You won!")}
-if(userHealth === "0");
-console.log("You lose!");}
+    
+    
+    
+    
+    
+if(enemyHealth === "0" && wins === "3"){
+console.log("You won!");
+}
+if(userHealth === "0");{
+  console.log("You lose!");
+}
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
